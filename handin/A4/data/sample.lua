df458684700a4a38fc54ca061e75f1b89c84963e
nelk <nelkishere@gmail.com>
2014-06-23 00:20:35 -0400

    A4 mesh drawing (and found the dodecahedron).

--- handin/A4/data/sample.lua
+++ handin/A4/data/sample.lua
@@ -4,21 +4,58 @@ mat1 = gr.material({0.7, 1.0, 0.7}, {0.8, 0.8, 0.8}, 30)
 
 scene_root = gr.node('root')
 
-s1 = gr.nh_sphere('s1', {0, 0, -80}, 6)
-scene_root:add_child(s1)
-s1:set_material(mat1)
+--s1 = gr.nh_sphere('s1', {0, 0, -80}, 6)
+--scene_root:add_child(s1)
+--s1:set_material(mat1)
 
-s2 = gr.nh_sphere('s2', {5, 5, -100}, 6)
-scene_root:add_child(s2)
-s2:set_material(mat1)
+--s2 = gr.nh_sphere('s2', {5, 5, -100}, 6)
+--scene_root:add_child(s2)
+--s2:set_material(mat1)
 
-b1 = gr.nh_box('b1', {-4, 3, -60}, 3)
-scene_root:add_child(b1)
-b1:set_material(mat1)
+--b1 = gr.nh_box('b1', {-4, 3, -60}, 3)
+--scene_root:add_child(b1)
+--b1:set_material(mat1)
+
+require('smstdodeca')
+
+steldodec:set_material(mat1)
+scene_root:add_child(steldodec)
+
+--triangle = gr.mesh('triangle', {
+  --{ 0, 0, -15},
+  --{ 0, 1, -15},
+  --{ 1, 1, -10},
+  --{ 1, 0, -10}
+--}, {
+  --{ 3, 2, 1, 0}
+--})
+--triangle:set_material(mat1)
+--scene_root:add_child(triangle)
+
+--thing = gr.mesh('thing', {
+  --{ -158.333350, 225.647350, -64.699450},
+  --{ -207.117500, 241.498300, -64.699450},
+  --{ -176.967250, 200.000000, -64.699450}
+--}, {
+  --{ 0, 1, 2}
+--})
+--thing:set_material(mat1)
+--scene_root:add_child(thing)
 
 --white_light = gr.light({-100.0, 150.0, 400.0}, {0.5, 0.5, 0.5}, {1, 0, 0})
 white_light = gr.light({-10, 0, -2}, {0.5, 0.5, 0.5}, {1, 0.0000000001, 0.0000001})
 
+-- Straight-on raw dodec
 gr.render(scene_root, 'sample.png', 1024, 768,
-  {-15, 10, 0}, {0.2, -0.08, -1}, {0, 1, 0}, 50,
+  {-140, 200, 600}, {0, 0, -1}, {0, 1, 0}, 50,
   {0.1, 0.1, 0.1}, {white_light})
+
+-- Side view raw dodec
+--gr.render(scene_root, 'sample.png', 256, 256,
+  --{200, -200, 200}, {-1.1, 1.1, -1}, {0, 1, 0}, 50,
+  --{0.1, 0.1, 0.1}, {white_light})
+
+--gr.render(scene_root, 'sample.png', 1024, 768,
+  --{-15, 10, 0}, {0.2, -0.08, -1}, {0, 1, 0}, 50,
+  --{0.1, 0.1, 0.1}, {white_light})
+
--- handin/A4/src/mesh.cpp
+++ handin/A4/src/mesh.cpp
@@ -29,6 +29,90 @@ std::ostream& operator<<(std::ostream& out, const Mesh& mesh) {
 
 
 std::vector<Intersection> Mesh::findIntersections(const Ray& ray) {
-  return std::vector<Intersection>();
+  std::vector<Intersection> intersections;
+
+  // Convext angle tests.
+  // TODO: Ray casting algorithm if we have convex faces.
+  /*
+  const double EPSILON = 0.0;
+  for (std::vector<Face>::const_iterator it = m_faces.begin(); it != m_faces.end(); it++) {
+    const Face& face = *it;
+    if (face.size() < 3) continue;
+    Vector3D normal;
+    Vector3D lastV = m_verts[face[face.size() - 1]] - m_verts[face[0]];
+    Vector3D currentV = m_verts[face[0]] - m_verts[face[1]];
+    normal = (-1 * lastV).cross(currentV);
+    normal.normalize();
+
+    double t = -(ray.pos[X] + ray.pos[Y] + ray.pos[Z])/(normal[X]*ray.dir[X] + normal[Y]*ray.dir[Y] + normal[Z]*ray.dir[Z]);
+    if (t < EPSILON) {
+      //break; // TODO
+      continue; // Pointing away from face.
+    }
+    Point3D p = ray.pos + t*ray.dir;
+
+    for (unsigned int i = 0; i < face.size(); i++) {
+      if (i > 0) {
+        currentV = m_verts[face[i]] - m_verts[face[i == face.size() - 1 ? 0 : i + 1]];
+      }
+
+      double thedot = lastV.dot(p - m_verts[face[i]]);
+      if (lastV.dot(currentV) < thedot) {
+        //break; // TODO
+        continue; // Point outside.
+      }
+
+      lastV = currentV;
+    }
+
+    intersections.push_back(Intersection(t, normal, NULL));
+    //break; // TODO
+  }
+  */
+
+  // Reinier van Vliet and Remco Lam angle sums algorithm.
+  const double EPSILON = 0.0000001;
+  for (std::vector<Face>::const_iterator it = m_faces.begin(); it != m_faces.end(); it++) {
+    const Face& face = *it;
+    if (face.size() < 3) {
+      std::cout << "Warn: Face with less than 3 verts found." << std::endl;
+      continue;
+    }
+
+    Vector3D v1 = m_verts[face[1]] - m_verts[face[0]];
+    Vector3D v2 = m_verts[face[1]] - m_verts[face[2]];
+    //Vector3D normal = v1.cross(v2);
+    Vector3D normal = v2.cross(v1);
+    normal.normalize();
+
+    //double t = (ray.pos[X] + ray.pos[Y] + ray.pos[Z])/(normal[X]*ray.dir[X] + normal[Y]*ray.dir[Y] + normal[Z]*ray.dir[Z]);
+    double t = -(ray.pos - m_verts[face[0]]).dot(normal) / ray.dir.dot(normal);
+    if (t < EPSILON) {
+      //std::cout << "Jump ship! t=" << t << std::endl;
+      continue; // Pointing away from face.
+    }
+    Point3D q = ray.pos + t*ray.dir;
+
+    double anglesum = 0.0;
+    for (unsigned int i = 0; i < face.size(); i++) {
+      Vector3D p1 = m_verts[face[i]] - q;
+      Vector3D p2 = m_verts[face[(i+1)%face.size()]] - q;
+
+      double m1 = p1.length();
+      double m2 = p2.length();
+      if (m1*m2 <= EPSILON) {
+        anglesum = M_PI*2; // We are on a node, consider this inside.
+      } else {
+        anglesum += acos(p1.dot(p2) / (m1*m2));
+      }
+      if (anglesum >= M_PI*2 - EPSILON && anglesum <= M_PI*2 + EPSILON) {
+        intersections.push_back(Intersection(t, normal, NULL));
+        //std::cout << "INTERSECTION " << t << std::endl;
+      }
+    }
+    //std::cout << "Exitted with anglesum=" << anglesum << std::endl;
+  }
+
+  return intersections;
 }
 
