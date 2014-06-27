e68042e6978bcb1d46a7d373ef799b68eab4e7d0
nelk <nelkishere@gmail.com>
2014-06-27 01:55:40 -0400

    Reverting provided scene scripts.

--- handin/A4/data/nonhier.lua
+++ handin/A4/data/nonhier.lua
@@ -1,9 +1,9 @@
 -- A simple scene with some miscellaneous geometry.
 
-mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25, 0.3)
-mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25, 0.3)
-mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25, 0.5)
-mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25, 0.8)
+mat1 = gr.material({0.7, 1.0, 0.7}, {0.5, 0.7, 0.5}, 25)
+mat2 = gr.material({0.5, 0.5, 0.5}, {0.5, 0.7, 0.5}, 25)
+mat3 = gr.material({1.0, 0.6, 0.1}, {0.5, 0.7, 0.5}, 25)
+mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 
 scene_root = gr.node('root')
 
@@ -41,7 +41,6 @@ scene_root:add_child(steldodec)
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.9, 0.9, 0.9}, {1, 0, 0})
 orange_light = gr.light({400.0, 100.0, 150.0}, {0.7, 0.0, 0.7}, {1, 0, 0})
 
---gr.render(scene_root, 'nonhier.png', 256, 256,
-gr.render(scene_root, 'nonhier.png', 1920, 1080,
-  {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
-  {0.3, 0.3, 0.3}, {white_light, orange_light})
+gr.render(scene_root, 'nonhier.png', 256, 256,
+	  {0, 0, 800}, {0, 0, -1}, {0, 1, 0}, 50,
+	  {0.3, 0.3, 0.3}, {white_light, orange_light})
