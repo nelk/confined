bda1e53a1f2d46e1d16a9b49d054fd99a6ed9178
nelk <nelkishere@gmail.com>
2014-06-27 01:46:26 -0400

    Tweak sample.

--- handin/A4/data/sample.lua
+++ handin/A4/data/sample.lua
@@ -1,52 +1,15 @@
 
+-- Sample scene, by Alex Klen.
+
 dodec_mat = gr.material({0.9, 0.6, 0.3}, {0.5, 0.5, 0.5}, 15, 0.1)
 ground = gr.material({0.4, 0.4, 0.4}, {0.8, 0.8, 0.8}, 25, 0.3)
 mirror = gr.material({1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, 40, 0.5)
 
---whitemat = gr.material({1.0, 1.0, 1.0}, {1.0, 1.0, 1.0}, 10)
-
 scene = gr.node('root')
-
---require('mickey')
---mickey:set_material(mat1)
---m1 = gr.node('m1')
---scene:add_child(m1)
---m1:add_child(mickey)
---m1:translate(-0.5, -0.5, -5)
---m1:rotate('Y', -90)
---m1:rotate('X', -90)
---m1:scale(2, 2, 2)
-
---require('cylinder')
---cyl:set_material(mirror)
-
---cyl_number = 1
---for _, x in pairs({-2, -1, 0, 1, 2}) do
-  --cyl_instance = gr.node('cyl' .. tostring(c_number))
-  --scene:add_child(cyl_instance)
-  --cyl_instance:add_child(cyl)
-  --cyl_instance:translate(x, -1, -6)
-  --cyl_instance:scale(0.3, 3.0, 0.3)
-  --cyl_number = cyl_number + 1
---end
-
---mirror_sphere = gr.sphere('mirror_sphere')
---mirror_sphere:set_material(mirror)
-
---s_number = 1
---for _, x in pairs({-2.5, -1.5, -0.5, 0.5, 1.5, 2.5}) do
-  --s_instance = gr.node('s' .. tostring(s_number))
-  --scene:add_child(s_instance)
-  --s_instance:add_child(mirror_sphere)
-  --s_instance:translate(x, -1, -6)
-  --s_instance:scale(0.3, 0.3, 0.3)
-  --s_number = s_number + 1
---end
 
 require('smstdodeca')
 steldodec:set_material(dodec_mat)
 steldodec:translate(150, -150, -100)
---steldodec:scale(0.4, 0.4, 0.4)
 
 d1 = gr.node('dodec1')
 d1:add_child(steldodec)
@@ -57,6 +20,7 @@ d2 = gr.node('dodec2')
 d2:add_child(steldodec)
 scene:add_child(d2)
 d2:translate(150, 150, -400)
+d2:rotate('Z', 30)
 
 
 s_node = gr.node('the_spheres')
@@ -91,10 +55,10 @@ platform:scale(20, 1, 20)
 white_light = gr.light({-100.0, 150.0, 400.0}, {0.5, 0.5, 0.5}, {1, 0, 0})
 red_light = gr.light({-5, -1, -5}, {1.0, 0.0, 0.0}, {1, 0, 0})
 green_light = gr.light({5, -1, -5}, {0.0, 1.0, 0.0}, {1, 0, 0})
---blue_light = gr.light({0, 10, -5}, {0.0, 0.0, 1.0}, {1, 0, 0})
 blue_light = gr.light({0, 2, -5}, {0.0, 0.0, 1.0}, {1, 0, 0})
 
-gr.render(scene, 'sample.png', 256, 256, --1920, 1080,
+-- Feel free to change the resolution.
+gr.render(scene, 'sample.png', 1920, 1080,
   {0, 0, 6}, {0, 0, -1}, {0, 1, 0}, 50,
   {0.3, 0.3, 0.3}, {red_light, green_light, blue_light}
 )
