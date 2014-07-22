b7879934ad49c36ee0ac83cd677f892d69674cd5
nelk <nelkishere@gmail.com>
2014-07-21 20:12:35 -0400

    Actually commit the settings files.

---
+++ handin/A5/confined/src/settings.cpp
@@ -0,0 +1,14 @@
+
+#include "settings.hpp"
+
+const std::string Settings::settingNames[] = {
+  "LIGHT_DIFFUSE",
+  "LIGHT_SPECULAR",
+  "SHADOW_MAP",
+  "NORMAL_MAP",
+  "TEXTURE_MAP",
+  "SSAO",
+  "BLUR",
+  "MOTION_BLUR"
+};
+
